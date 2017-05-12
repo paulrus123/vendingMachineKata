cfd3e308a308ad837ec7af5c44731db898cb2249
paulrus123 <Paul@Pauls-MacBook-Pro.local>
2017-05-11 22:41:28 -0400

    Initial Commit

---
+++ VendingMachineKata/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VendingMachineKata
+//
+//  Created by Paul Sorenson on 5/11/17.
+//  Copyright © 2017 Paul Sorenson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
